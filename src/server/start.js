feb2be8f3067cba2a4fdfc1708a8e91c33abc931
Dhanfia <dhanfiarahman@gmail.com>
2021-08-23 18:43:14 +0200

    Reverted

--- src/server/start.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server.js')
-
-const port = 8081;
-const server = app.listen(port, () => {
-    console.log('server running');
-    console.log(`running on localhost: ${port}`);
-})
